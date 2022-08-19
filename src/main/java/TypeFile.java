925e6e106ecd6b040846755cdc0bf89c515566be
tariq-almalki <e.tariq.almalki@gmail.com>
2022-08-19 15:59:20 +0300

    add feature hashing files

--- src/main/java/TypeFile.java
+++ src/main/java/TypeFile.java
@@ -1,11 +1,18 @@
+import com.password4j.Password;
+import org.apache.commons.codec.digest.DigestUtils;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
-import org.apache.commons.codec.digest.DigestUtils;
+import java.util.stream.Collectors;
+
 
 @Command(name = "file", description = "hashing files with various algorithms")
 public class TypeFile implements Callable<Integer> {
@@ -13,13 +20,39 @@ public class TypeFile implements Callable<Integer> {
     @Parameters(paramLabel = "FILE", index = "0", arity = "1", description = "file to be hashed")
     private File file;
 
-    @Option(names = {"-a", "--algorithm"}, arity = "0..1", description = "hashing functions")
+    @Option(names = {"-a", "--algorithm"}, arity = "0..1", description = "SHA-1\n" +
+            "SHA-256\n" +
+            "SHA-384\n" +
+            "SHA-512\n" +
+            "SHA-512/224\n" +
+            "SHA-512/256\n" +
+            "SHA3-224\n" +
+            "SHA3-256\n" +
+            "SHA3-384\n" +
+            "SHA3-512\n")
     String algorithm = "SHA1-256";
 
     @Override
     public Integer call() throws Exception {
         byte[] bytes = Files.readAllBytes(file.toPath());
 
+        //noinspection DuplicatedCode
+        Path path = Paths.get("./hashing-functions/hashing_functions.txt");
+
+        List<String> listOfHashingFunctions = Files.readAllLines(path);
+
+        Map<String, String> hashingFunctions = listOfHashingFunctions
+                .stream()
+                .collect(Collectors.toMap(str -> str.split(",\\s+")[1].toLowerCase(), str -> str.split(",\\s+")[0]));
+
+
+        String hashed = (String) DigestUtils
+                .class
+                // we pass "String.class" to "getMethod" so it knows beforehand parameter's type that will be used for invoking
+                // the method.
+                .getMethod(hashingFunctions.get(algorithm.toLowerCase()), byte[].class).invoke(null, bytes);
+
+        System.out.printf("message digest: %s%n", hashed);
 
         return 0;
     }
--- src/main/java/TypeText.java
+++ src/main/java/TypeText.java
@@ -5,11 +5,9 @@ import picocli.CommandLine.Command;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -22,6 +20,7 @@ public class TypeText implements Callable<Integer> {
     @Parameters(paramLabel = "STR", index = "0", arity = "0..1", description = "string to be hashed")
     private String str;
 
+    @SuppressWarnings("FieldMayBeFinal")
     @Option(names = {"-a", "--algorithm"}, arity = "0..1", description = "SHA-1\n" +
             "SHA-256\n" +
             "SHA-384\n" +
@@ -31,31 +30,28 @@ public class TypeText implements Callable<Integer> {
             "SHA3-224\n" +
             "SHA3-256\n" +
             "SHA3-384\n" +
-            "SHA3-512\n" +
-            "Argon2\n" +
-            "bcrypt\n" +
-            "scrypt\n" +
-            "PBKDF2")
+            "SHA3-512\n")
     private String algorithm = "SHA-256";
 
 
     @Override
     public Integer call() throws Exception {
 
-        Path path = Paths.get("commons-codec.txt");
+        //noinspection DuplicatedCode
+        Path path = Paths.get("./hashing-functions/hashing_functions.txt");
 
         List<String> listOfHashingFunctions = Files.readAllLines(path);
 
-        Map<String, String> hashing_functions = listOfHashingFunctions
+        Map<String, String> hashingFunctions = listOfHashingFunctions
                 .stream()
-                .collect(Collectors.toMap(str->str.split(",\\s+")[1].toLowerCase(), str -> str.split(",\\s+")[0]));
+                .collect(Collectors.toMap(str -> str.split(",\\s+")[1].toLowerCase(), str -> str.split(",\\s+")[0]));
 
 
         String hashed = (String) DigestUtils
                 .class
                 // we pass "String.class" to "getMethod" so it knows beforehand parameter's type that will be used for invoking
                 // the method.
-                .getMethod(hashing_functions.get(algorithm.toLowerCase()), String.class)
+                .getMethod(hashingFunctions.get(algorithm.toLowerCase()), String.class)
                 .invoke(null, str);
 
         System.out.printf("message digest: %s%n", hashed);
